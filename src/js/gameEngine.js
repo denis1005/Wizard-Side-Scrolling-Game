f4ccf8f7388d554e4937849ae58c641ce120b70e
Denislav <denis1005@users.noreply.github.com>
2023-01-11 14:02:33 +0200

    Adding movement to all direction

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -12,8 +12,24 @@ function gameLoop (state,game){
     wizard.positionX+=4;
    }
 
+   if (state.keys.KeyW){
+    wizard.positionY-=4;
+   }
+
+   if (state.keys.KeyS){
+    wizard.positionY+=4;
+   }
+
+   if (state.keys.KeyA){
+    wizard.positionX-=4;
+   }
+
   // Render
   wizardElement.style.left= wizard.positionX + 'px';
+  wizardElement.style.top= wizard.positionY + 'px';
+  wizardElement.style.bottom= wizard.positionY + 'px';
+  wizardElement.style.right= wizard.positionY + 'px';
+  
 
   window.requestAnimationFrame(gameLoop.bind(null,state,game))
 }
