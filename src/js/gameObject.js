3b3f58681f111deef3a2799945d9e8590645b509
Denislav <denis1005@users.noreply.github.com>
2023-01-11 13:10:53 +0200

    Create wizard with initial position with start

--- src/js/gameObject.js
+++ src/js/gameObject.js
@@ -5,6 +5,17 @@ const gameScreen=document.querySelector('.game-screen');
 function initGameObject(){
    return {
     startScreen,
-    gameScreen
+    gameScreen,
+     createWizard(initialState){
+       let wizardElement=document.createElement('div');
+       wizardElement.classList.add('wizard');
+       wizardElement.style.width=initialState.width+'px';
+       wizardElement.style.height=initialState.height+'px';
+       wizardElement.style.left=initialState.startX +'px'
+       wizardElement.style.top=initialState.startY +'px'
+       this.wizardElement=wizardElement;
+       gameScreen.appendChild(wizardElement);
+       return wizardElement;
+     }
    }
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,6 +1,11 @@
 function initState(){
     const state={
-
+        wizard:{
+            width:82,
+            height:100,
+            startX:100,
+            startY:200,
+        }
     }
     return state;
 }
