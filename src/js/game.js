14ea4e1086013b1edc01d265239be69273a4ae99
Denislav <denis1005@users.noreply.github.com>
2023-01-11 12:28:27 +0200

    Add game engine

--- src/js/game.js
+++ src/js/game.js
@@ -1,8 +1,12 @@
+
 let state=initState();
 let game=initGameObject();
-
 game.startScreen.addEventListener('click',(e)=>{
     console.log('start');
     game.startScreen.classList.add('hidden');
     game.gameScreen.classList.remove('hidden')
+
+    // Start game
+   start(state,game);
 })
+
